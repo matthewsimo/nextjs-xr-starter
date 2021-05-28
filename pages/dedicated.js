8c6d2925cb06b43b6ef45c62ac8042f8fabd4b14
Matthew Simo <matthew.a.simo@gmail.com>
2021-05-28 17:01:25 -0500

    scaffold in preparation

---
+++ pages/dedicated.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Dedicated() {
+  return (
+    <div>
+      <Head>
+        <title>Dedicated Page Example</title>
+        <meta
+          name="description"
+          content="React XR Starter: Dedicated Page Example"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Dedicated Example</h1>
+        {/* Add Canvas Here */}
+      </main>
+    </div>
+  );
+}
